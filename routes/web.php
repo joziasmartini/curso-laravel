6bf457911d710cdbf24a91da1b60fc4f4462e8d7
Jozias Martini <jozias.martini@gmail.com>
2020-09-13 13:38:10 -0300

    Update, lesson twelve

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,47 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Route::view('/view', 'welcome');
+
+Route::redirect('/redirect1', '/redirect2');
+
+// Route::get('/redirect1', function () {
+//     return redirect('/redirect2');
+// });
+
+Route::get('/redirect2', function () {
+    return "Redirect 2!";
+});
+
+Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
+    return "Produtos: {$idProduct}";
+});
+
+Route::get('/categorias/{flag}/posts', function ($flag) {
+    return "Posts da categoria: {$flag}";
+});
+
+Route::get('/categoria/{flag}', function ($flag) {
+    return "Produtos da categoria: {$flag}";
+});
+
+Route::match(['put', 'post'],'/match', function () {
+    return 'Match';
+});
+
+Route::any('/any', function () {
+    return 'Any';
+});
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/contato', function () {
+    return view('contact');
+});
+
+Route::get('/ola', function () {
+    return 'Olá!';
 });
