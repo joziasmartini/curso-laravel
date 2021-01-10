e2f63e0ae8a58880d6703940870dd618740fceb8
Jozias Martini <jozias.martini@gmail.com>
2021-01-10 17:55:36 -0300

    Class eighteen, crud controllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,8 +7,32 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index() {
-        $products = ['Product 01', 'Product 02', 'Product 03'];
+        $products = ['Produto 1', 'Produto 2', 'Produto 3'];
 
         return $products;
+    }
+
+    public function show($id) {
+        return "Exibir o produto de id: {$id}";
+    }
+
+    public function create() {
+        return "Exibir o formulário de cadastrar produto";
+    }
+
+    public function edit($id) {
+        return "Exibir o formulário de editar produto de id: {$id}";
+    }
+
+    public function store() {
+        return "Cadastrar um produto";
+    }
+
+    public function update($id) {
+        return "Editar um produto";
+    }
+
+    public function delete($id) {
+        return "Deletar um produto";
     }
 }
